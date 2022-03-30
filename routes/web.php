2116b9ac04afa37b6fd384087da07763dfafa38b
M7MDAKRAM <mhmd2000akram@gmail.com>
2022-03-30 10:57:46 +0300

    First

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,45 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/about', function(){
+$name ='ahmed';
+$age ='20';
+
+return view('about')->with('name',$name)->with('age',$age);
+
+});
+
+Route::get('tasks', function () {
+    $tasks= [
+        '1'=>'task 1',
+        '2'=>'task 2',
+        '3'=> 'task 3',
+    ];
+    return view('tasks',compact('tasks'));
+});
+
+Route::get('/show/{id}', function ($id) {
+    $tasks = [
+      '1'=>  'task 1',
+      '2'=>  'task 2',
+      '3'=> 'task 3',
+    ];
+
+    $task = $tasks[$id];
+    return view('tasks',compact('task'));
+});
+
+
+Route::post('/tasks', function () {
+    $name = $_POST['name'];
+    return view('tasks',compact('name'));
+});
+
+// Route::get('about' ,function(){
+//     $name = $_POST['name'];
+//    return view('about' ,compact('name'));
+
+//  });
